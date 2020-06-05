e909691add754a80c68cce26e9575740a367d849
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-06-05 12:41:03 +0400

    added models to the users app)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin,AbstractUser
+from guardian import mixins
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_countries.fields import CountryField
+from django.utils import timezone
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+GENDER_CHOICES = [
+    ('male', 'Male'),
+    ('female', 'Female')
+]
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser,mixins.GuardianUserMixin): 
+    email =  models.EmailField(_('email_address'), unique=True, name='email')
+    username =  models.CharField(_('username'), unique=True, max_length=128)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    birth_date = models.DateField(verbose_name='birth_date',blank=True,null=True)
+    gender = models.CharField(max_length=10,choices=GENDER_CHOICES)
+    slug = models.SlugField(max_length=255,unique=True,null=True,blank=True)
+    country = CountryField()
+    city = models.CharField(max_length=255,null=True,blank=True)
+    bio = models.CharField(max_length=300)
+    image = models.ImageField(verbose_name='userimages',upload_to='user_images',null=True,blank=True)
+
+    def get_picture(self):
+        default_picture = settings.STATIC_URL + 'img/default_picture.png'
+        if self.image:
+            return self.image.url
+        else:
+            return default_picture
+
+    def __str__(self):
+        return self.user.email
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
